150369fea4189e4f7d77fd825222f35c6b53478f
lemon9921 <297405659@qq.com>
2016-04-12 18:45:30 +0800

    mem.py

--- mymem.py
+++ mymem.py
@@ -3,6 +3,7 @@
 
 import socket
 import json
+import time
 
 
 RECV_SIZE = 4096
@@ -10,38 +11,38 @@ RECV_SIZE = 4096
 
 class Client(object):
 	def __init__(self,
-	             server,
-	             timeout=None):
-	    self.sock = None
-	    self.server = server
-	    self.timeout = timeout
+				 server,
+				 timeout=None):
+		self.sock = None
+		self.server = server
+		self.timeout = timeout
 
 	def _connect(self):
-	    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	    sock.connect(self.server)
-	    sock.settimeout(self.timeout)
-	    self.sock = sock
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect(self.server)
+		sock.settimeout(self.timeout)
+		self.sock = sock
 
 	def _close(self):
-	    if self.sock:
-	        self.sock.close()
-	    self.sock = None
+		if self.sock:
+			self.sock.close()
+		self.sock = None
 
 	def get(self, name):
-	    name += b''
-	    cmd = name + b'\r\n'
-	    if not self.sock:
-	        self._connect()
-	    self.sock.sendall(cmd)
+		name += b''
+		cmd = name + b'\r\n'
+		if not self.sock:
+			self._connect()
+		self.sock.sendall(cmd)
 
-	    buf = b''
-	    result = []
-	    while True:
-	        buf, line = parseline(self.sock, buf)
-	        if line == b'END':
-	            self._close()
-	            return result
-	        result.append(line)
+		buf = b''
+		result = []
+		while True:
+			buf, line = parseline(self.sock, buf)
+			if line == b'END':
+				self._close()
+				return result
+			result.append(line)
 
 
 def parseline(sock, buf):
@@ -49,23 +50,23 @@ def parseline(sock, buf):
 	last_char = b''
 
 	while True:
-	    if last_char == b'\r' and buf[0:1] == b'\n':
-	        # Strip the last character from the last chunk.
-	        chunks[-1] = chunks[-1][:-1]
-	        return buf[1:], b''.join(chunks)
-	    elif buf.find(b'\r\n') != -1:
-	        before, sep, after = buf.partition(b"\r\n")
-	        chunks.append(before)
-	        return after, b''.join(chunks)
+		if last_char == b'\r' and buf[0:1] == b'\n':
+			# Strip the last character from the last chunk.
+			chunks[-1] = chunks[-1][:-1]
+			return buf[1:], b''.join(chunks)
+		elif buf.find(b'\r\n') != -1:
+			before, sep, after = buf.partition(b"\r\n")
+			chunks.append(before)
+			return after, b''.join(chunks)
 
-	    if buf:
-	        chunks.append(buf)
-	        last_char = buf[-1:]
+		if buf:
+			chunks.append(buf)
+			last_char = buf[-1:]
 
-	    try:
-	        buf = sock.recv(RECV_SIZE)
-	    except IOError as e:
-	        raise e
+		try:
+			buf = sock.recv(RECV_SIZE)
+		except IOError as e:
+			raise e
 
 
 def  mget_stats():
@@ -82,20 +83,29 @@ def  mget_stats():
 		stats[k]=v
 		
 	data = []
-	stats['usage'] = str(100 * float(stats['bytes']) / float(stats['limit_maxbytes']))
+	stats['usage'] = '%.2f' % float(100 * float(stats['bytes']) / float(stats['limit_maxbytes']))
 	try:
-		stats['get_hit_ratio'] = str(100 * float(stats['get_hits']) / (float(stats['get_hits']) + float(stats['get_misses'])))
+		stats['get_hit_ratio'] = '%.2f' %float(100 * float(stats['get_hits']) / (float(stats['get_hits']) + float(stats['get_misses'])))
 	except ZeroDivisionError:
 		stats['get_hit_ratio'] = '0.0'
-	for i in 'usage','get_hit_ratio','evictions':
-		dict = {i:stats[i]}
+	for key in 'usage','get_hit_ratio','evictions':
+		#dict = {i:stats[i]}
+		dict =  {
+				'metric': '%s.%s%s' % (metric, key, suffix),
+				'endpoint': endpoint,
+				'timestamp': timestamp,
+				'step': step,
+				'value': stats[key],
+				'counterType': 'COUNTER',
+				'tags': 'global status'
+			}
 		data.append(dict)
 	return data
 
 
 def  mget_slabs():
 	"""
-   	   @return: memcache slabs dict
+   	   @return: memcache slabs list
 	"""
 	c = Client((host,port), timeout=5)
 	slabs = c.get("stats slabs")
@@ -107,11 +117,30 @@ def  mget_slabs():
 		slab_name = slabs_one[0].split(':')[0]
 		k = slabs_one[2].split()[1].split(':')[1]
 		v = slabs_one[2].split()[-1]
-		dict1 = {k:v}
+		#dict1 = {k:v}
+		dict1 =  {
+				'metric': '%s.%s%s' % (metric, k, suffix),
+				'endpoint': endpoint,
+				'timestamp': timestamp,
+				'step': step,
+				'value': v,
+				'counterType': 'COUNTER',
+				'tags': 'class slab value'
+			}
 		k1 = slabs_one[4].split()[1].split(':')[1]
 		v1 = 100*(float(slabs_one[4].split()[-1]) / float(slabs_one[3].split()[-1]))
 		dict1[k1 + '_percent'] = '%.2f' %v1  
-		dict2 = {slab_name:dict1}
+		#dict2 = {slab_name:dict1}
+		dict2 =  {
+				'metric': '%s.%s%s' % (metric, 'slabs_', slab_name),
+				'endpoint': endpoint,
+				'timestamp': timestamp,
+				'step': step,
+				'value': dict1,
+				'counterType': 'COUNTER',
+				'tags': 'slab status'
+			}
+
 		data.append(dict2)
 	return data
 
@@ -124,7 +153,13 @@ def main():
 
 if __name__ == "__main__":
 
-	host=''
+	host='172.16.200.98'
 	port = 11211
+
+	timestamp = int(time.time())
+	step = 60
+	endpoint = host
+	metric = 'memcache'
+	suffix = '_value'
 
 	main()
