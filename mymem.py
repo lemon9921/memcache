02fa4a881d5c70c3f5afb99b488a9d4226861565
lemon9921 <297405659@qq.com>
2016-04-12 16:50:30 +0800

    mem.py

--- mymem.py
+++ mymem.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
+
 import socket
-import re,json
+import json
+
 
 RECV_SIZE = 4096
 
@@ -67,50 +69,62 @@ def parseline(sock, buf):
 
 
 def  mget_stats():
+	"""
+		@return: memcache stats dict
+	"""
 	c = Client((host,port), timeout=5)
-	#print c.get("stats slabs")
-	_stat_regex = re.compile(ur"STAT (.*) ([0-9]+\.?[0-9]*)")
 	stat = c.get("stats")
+
 	stats={}
 	for i in stat:
-		k=re.compile(ur"STAT (.*) ([0-9]+\.?[0-9]*)").findall(i)[0][0]
-		v=re.compile(ur"STAT (.*) ([0-9]+\.?[0-9]*)").findall(i)[0][1]
+		k=i.split()[1]
+		v=i.split()[2]
 		stats[k]=v
 		
-	return stats
-
-
-
-def  mget_slabs():
-	c = Client((host,port), timeout=5)
-	#print c.get("stats slabs")
-	_stat_regex = re.compile(ur"STAT (.*) ([0-9]+\.?[0-9]*)")
-	slab = c.get("stats slabs")
-	slabs={}
-	slabs_num=slab.count('total_pages')
-	for i in slab:
-		kv=re.compile(ur'(STAT \d+:\w+) (\d+)').findall(i)
-		for j in 'total_page','total_chunks','used_chunks' :
-			if str(kv).count(j):
-				k=re.compile(ur'(STAT \d+:\w+) (\d+)').findall(i)[0][0]
-				slabs[k]=re.compile(ur'(STAT \d+:\w+) (\d+)').findall(i)[0][1]
-	return slabs
-
-
-def main():
-	data=[]
-	stats=Mget_stats()
+	data = []
 	stats['usage'] = str(100 * float(stats['bytes']) / float(stats['limit_maxbytes']))
 	try:
 		stats['get_hit_ratio'] = str(100 * float(stats['get_hits']) / (float(stats['get_hits']) + float(stats['get_misses'])))
 	except ZeroDivisionError:
 		stats['get_hit_ratio'] = '0.0'
-	#print stats['get_hit_ratio']
-	i={'get_hit_ratio':stats['get_hit_ratio']}
-	data.append(i)
-	slabs=Mget_slabs()
-	print slabs
+	for i in 'usage','get_hit_ratio','evictions':
+		dict = {i:stats[i]}
+		data.append(dict)
+	return data
+
+
+def  mget_slabs():
+	"""
+   	   @return: memcache slabs list
+	"""
+	c = Client((host,port), timeout=5)
+	slabs = c.get("stats slabs")
+	
+	data = []
+	num_slabs = int(slabs[-2].split()[2])
+	for n in range(num_slabs):
+		slabs_one = slabs[16*n:16*(n+1)]
+		slab_name = slabs_one[0].split(':')[0]
+		k = slabs_one[2].split()[1].split(':')[1]
+		v = slabs_one[2].split()[-1]
+		dict1 = {k:v}
+		k1 = slabs_one[4].split()[1].split(':')[1]
+		v1 = 100*(float(slabs_one[4].split()[-1]) / float(slabs_one[3].split()[-1]))
+		dict1[k1 + '_percent'] = '%.2f' %v1  
+		dict2 = {slab_name:dict1}
+		data.append(dict2)
+	return data
+
+
+
+def main():
+	data = mget_stats() + mget_slabs()
 	print  json.dumps(data)
+
+
 if __name__ == "__main__":
+
+	host='172.16.200.98'
 	port = 11211
+
 	main()
